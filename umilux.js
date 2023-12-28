d803fbc9854b8317b7333271bc47c0f76e55f795
Dtian <tienanhdinh.work@gmail.com>
2023-12-28 17:44:07 +0700

    updating

--- umilux.js
+++ umilux.js
@@ -109,3 +109,29 @@ function disableScroll() {
 function enableScroll() {
     window.onscroll = function () { };
 }
+
+const days = document.querySelectorAll('days');
+const hours = document.querySelectorAll('hours');
+const minutes = document.querySelectorAll('minutes');
+const seconds = document.querySelectorAll('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date("Jan 5, 2024 15:37:25").getTime();
+
+function updateCountdown() {
+    const currentTime = new Date().getTime();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = d;
+    document.getElementById("hours").innerHTML = h < 10 ? '0' + h : h;
+    document.getElementById("minutes").innerHTML = m < 10 ? '0' + m : m;
+    document.getElementById("seconds").innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
